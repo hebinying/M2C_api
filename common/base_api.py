4085e2863a51546a629d980d7c83cb6d28a7a11d
hebinying <hthby@163.com>
2018-10-19 19:35:01 +0800

    tijiao

--- common/base_api.py
+++ common/base_api.py
@@ -1,4 +1,6 @@
+#coding=utf-8
 import json
+import xlrd
 import requests
 
 def send_requests(s,testdata):
@@ -14,17 +16,19 @@ def send_requests(s,testdata):
         header=testdata['headers']
     except:
         headers={'token':name}
-
-    type=testdata['type']
+    try:
+        type=testdata['type']
+    except:
+        type=""
     try:
         bodydata=testdata['body']
     except:
         bodydata={}
 
-    try:
-        verify=testdata['verify']
-    except:
-        verify=False
+    # try:
+    #     verify=testdata['verify']
+    # except:
+    #     verify=False
 
 
     #files参数如何传
@@ -37,29 +41,85 @@ def send_requests(s,testdata):
 
     #使用prepared与未使用的prepared的差别
 
-    req=requests.Request(method=method,
-                         url=url,
-                         params=param,
-                         headers=headers,
-                         data=body,
-                         verify=verify)
-    prepped=req.prepare()
-    prepped=s.prepare_request(req)
-    resp=s.send(prepped,
-                timeout=30)
+    # req=requests.Request(method=method,
+    #                      url=url,
+    #                      params=param,
+    #                      headers=headers,
+    #                      data=body)
+    # prepped=req.prepare()
+    # prepped=s.prepare_request(req)
+    # resp=s.send(prepped,
+    #             timeout=30)
 
 
+    resp=s.request(method=method,
+                   url=url,
+                   params=param)
     #对返回结果的操作
     res={}
     res["statuscode"]=str(resp.status_code)
     res["time"]=resp.elapsed.total_seconds()
+    #是否需要转码
+    res["context"]=resp.content
+    res["header"]=resp.headers
+    res["encoding"]=resp.encoding
+    res["url"]=resp.url
+    res["request"]=resp.request
+    if res["statuscode"]!="200":
+        res["error"]=res["context"]
+    else:
+        res["error"]=""
 
+    res["reason"]=resp.reason
+    res["cookie"]=resp.cookies
+    res["text"]=resp.text
+    return res
 
+def write_result(results,filepath="result.xlsx"):
+    wb=xlrd.open_workbook(filepath)
+    sheet0=wb.get_sheet(0)
+    for result in results:
+        row_num=result["row_num"]
+        #写入结果
+        sheet0.write(row_num,0,result["id"])
+        sheet0.write(row_num,1,result["url"])
+        sheet0.write(row_num,2,result["statuscode"])
 
 
 
 if __name__=='__main__':
     s=requests.session()
-    datas=({"a":1,"b":2},{"a":3,"b":2})
-    for data in datas:
-        send_requests(s,data)
+    params={"ie": "utf-8",
+            "newi": 1,
+            "mod": 1,
+            "isbd": 1,
+            "isid": "9968179c0004987a",
+            "wd": "requests",
+            "rsv_spt": 1,
+            "rsv_iqid": "0x9a3acd6a0004efbc",
+            "issp": 1,
+            "f": 3,
+            "rsv_bp": 1,
+            "rsv_idx": 2,
+            "ie": "utf-8",
+            "rqlang": "cn",
+            "tn": "baiduhome_pg",
+            "rsv_enter": 0,
+            "oq": "requests",
+            "rsv_t": "464619kWUtIBOVkBZ3t35Vh04PwL+HpjW1cOUN3JUuEyZ3jJYDdThDloB7mVTZh4LHdp",
+            "rsv_pq": "9968179c0004987a",
+            "prefixsug": "requests",
+            "rsp": 0,
+            "bs": "requests",
+            "rsv_sid": "1458_21122_18559_26350_20929",
+            "_ss": 1,
+            "clist": "6cdb785551fd62c4	2dfe1d6b086f751d",
+            "hsug":"",
+           "f4s": 1,
+            "csor": 8,
+            "_cr1": 35958}
+    data={'method': 'get',
+          'url':"https://www.baidu.com/s",
+          'param':{"wd": u"接口测试自动化"}
+          }
+    send_requests(s,data)
